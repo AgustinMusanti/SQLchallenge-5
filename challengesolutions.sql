148cdab2e5e58853cc5867076d2cbc9f05c51acd
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2025-02-22 16:58:28 -0300

    >>

--- challengesolutions.sql
+++ challengesolutions.sql
@@ -10,3 +10,14 @@ INNER JOIN clientes c  ON v.cliente_id = c.cliente_id
 INNER JOIN productos p ON v.producto_id = p.producto_id;
 
 
+## 2- LEFT JOIN
+
+SELECT 
+    c.nombre_cliente  AS cliente,
+    p.nombre_producto AS producto
+    
+FROM clientes c
+    
+LEFT JOIN ventas v ON c.cliente_id = v.cliente_id
+LEFT JOIN productos p ON v.producto_id = p.producto_id;
+
